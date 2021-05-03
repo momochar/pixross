df2b785bbda957eec50f2bcdaee06c8b7129e0a1
aksh-t <tanno.ao@gmail.com>
2021-05-03 14:28:19 +0900

    Guideコンポーネントにkeyを付与する

--- frontend/src/components/GuideGroup/index.tsx
+++ frontend/src/components/GuideGroup/index.tsx
@@ -1,8 +1,14 @@
 import Guide from "../Guide";
 import { Stack } from "@chakra-ui/react";
 
-function GuideGroup(props: { guides: number[]; direction: "column" | "row" }) {
-  const guideElement = props.guides.map((g) => <Guide guide={g}></Guide>);
+function GuideGroup(props: {
+  guides: number[];
+  index: number;
+  direction: "column" | "row";
+}) {
+  const guideElement = props.guides.map((g, i) => (
+    <Guide key={`${props.direction}_${props.index}_${i}`} guide={g}></Guide>
+  ));
   return (
     <Stack spacing="0px" direction={props.direction}>
       {guideElement}
