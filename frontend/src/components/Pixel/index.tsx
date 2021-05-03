f2aeff1d4db5116c316ec42acbea151fb29f032d
mayumi aka inukawaii <inukawaii999@gmail.com>
2021-05-03 15:29:25 +0900

    Boxに変更

--- frontend/src/components/Pixel/index.tsx
+++ frontend/src/components/Pixel/index.tsx
@@ -1,7 +1,8 @@
 import "./index.css";
+import { Box } from "@chakra-ui/react";
 
 function Pixel() {
-  return <div className="pixel"></div>;
+  return <Box bg="lightblue" w="100%" p={0}></Box>;
 }
 
 export default Pixel;
