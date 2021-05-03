ce1293217eb7206bb37b3a307b44ec0251250c8f
mayumi aka inukawaii <inukawaii999@gmail.com>
2021-05-03 15:38:21 +0900

    WIP

--- frontend/src/components/Pixel/index.test.tsx
+++ frontend/src/components/Pixel/index.test.tsx
@@ -1,6 +1,17 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Pixel from "./";
 
 test("renders Pixel", () => {
   render(<Pixel />);
 });
+
+describe("背景色について", () => {
+  test("defaultはwhite", () => {
+    render(<Pixel />);
+
+    screen.debug();
+  });
+  test("クリックされるとlightblueに変わる", () => {});
+  test("lightblueのboxがクリックされるとwhiteに戻る", () => {});
+
+})
