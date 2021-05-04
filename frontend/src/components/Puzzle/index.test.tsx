3089329c1e4fd98de6f8bab12ba6add8c30bfe30
mayumi aka inukawaii <inukawaii999@gmail.com>
2021-05-04 13:20:28 +0900

    テストパターンを追加

--- frontend/src/components/Puzzle/index.test.tsx
+++ frontend/src/components/Puzzle/index.test.tsx
@@ -10,16 +10,31 @@ test("renders Puzzle", () => {
   render(<Puzzle puzzle={puzzle} size={10} />);
 });
 
-test("createGuide", () => {
-  const puzzle = [
-    [1, 0, 1],
-    [0, 1, 0],
-    [1, 0, 1],
-  ];
-  const result = createGuide(puzzle);
-  const expected = {
-    column: [[1, 1], [1], [1, 1]],
-    row: [[1, 1], [1], [1, 1]]
-  }
-  expect(result).toStrictEqual(expected);
-});
+describe("#createGuide", () => {
+  test("パターン1 塗っているマスがつながっていない場合", () => {
+    const puzzle = [
+      [1, 0, 1],
+      [0, 1, 0],
+      [1, 0, 1],
+    ];
+    const result = createGuide(puzzle);
+    const expected = {
+      column: [[1, 1], [1], [1, 1]],
+      row: [[1, 1], [1], [1, 1]]
+    }
+    expect(result).toStrictEqual(expected);
+  });
+    test("パターン2 塗っているマスがつながっている場合", () => {
+      const puzzle = [
+        [1, 1, 1],
+        [0, 1, 0],
+        [1, 1, 1],
+      ];
+      const result = createGuide(puzzle);
+      const expected = {
+        column: [[1, 1], [3], [1, 1]],
+        row: [[3], [1], [3]],
+      };
+      expect(result).toStrictEqual(expected);
+    });
+})
