b3e6e541c0e1cf0decb0cdd52747584a8d99eb82
aksh-t <tanno.ao@gmail.com>
2021-05-04 15:21:06 +0900

    PixelにrowIndexとcolumnIndexを渡す

--- frontend/src/components/Board/index.tsx
+++ frontend/src/components/Board/index.tsx
@@ -3,9 +3,19 @@ import { SimpleGrid } from "@chakra-ui/react";
 import { PixelStatus } from "../../types";
 
 function Board(props: { size: number; statuses: PixelStatus[][] }) {
-  const listItems = props.statuses.flat().map((status, i) => (
-    <Pixel key={i} status={status}></Pixel>
-  ));
+  const listItems = props.statuses
+    .map((row, rowIndex) =>
+      row.map((status, columnIndex) => (
+        <Pixel
+          key={`${rowIndex}_${columnIndex}`}
+          rowIndex={rowIndex}
+          columnIndex={columnIndex}
+          status={status}
+          onStatusChange={onStatusChange}
+        ></Pixel>
+      ))
+    )
+    .flat();
 
   return <SimpleGrid columns={props.size}>{listItems}</SimpleGrid>;
 }
