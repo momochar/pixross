c173dc6afa4afb2b6e41e23a1f522ee63b5806ef
aksh-t <tanno.ao@gmail.com>
2021-05-04 15:21:29 +0900

    WIP: BoardのonStatusChangeを実装

--- frontend/src/components/Board/index.tsx
+++ frontend/src/components/Board/index.tsx
@@ -3,6 +3,17 @@ import { SimpleGrid } from "@chakra-ui/react";
 import { PixelStatus } from "../../types";
 
 function Board(props: { size: number; statuses: PixelStatus[][] }) {
+  const onStatusChange = (
+    rowIndex: number,
+    columnIndex: number,
+    nextStatus: PixelStatus
+  ) => {
+    // TODO: 親であるPuzzleから更新関数を受け取って、それを呼び出す
+    console.log(rowIndex);
+    console.log(columnIndex);
+    console.log(nextStatus);
+  };
+
   const listItems = props.statuses
     .map((row, rowIndex) =>
       row.map((status, columnIndex) => (
